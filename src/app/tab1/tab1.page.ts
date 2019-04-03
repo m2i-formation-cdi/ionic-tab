110e9fd4eabccb19db325e48e71aa848b56fb438
Nazim Hickmet <toto@mail.com>
2019-04-03 10:35:36 +0200

    liste de random users

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+
+  public userList:Array<any> = [];
+
+  constructor(private http: HttpClient) { 
+
+    let url = "https://randomuser.me/api?results=20";
+
+    let req = this.http.get(url);
+
+    req.subscribe(
+      (data:any)=> {
+        console.log(data);
+        this.userList = data.results;
+      }
+    )
+  }
+
+}
